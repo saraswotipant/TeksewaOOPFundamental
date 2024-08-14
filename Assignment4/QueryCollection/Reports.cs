99fc2ab1e8685671547cbcc420612b3b37f071e5
Saraswoti Pant <sspant01@gmail.com>
2024-08-14 20:02:41 +0545

    Assignment 4 , query collection using linq

---
+++ Assignment4/QueryCollection/Reports.cs
@@ -0,0 +1,65 @@
+﻿// File - Reports.cs
+
+internal class Reports
+{
+	static List<Trainee> trainees = [
+        new("Ram Bista", new DateTime(1990, 12, 12), "Vedas", "BIT"),
+        new("Laxman Shrestha", new DateTime(1988, 1, 10), "Ascol", "BSC"),
+        new("Bishnu Rawal", new DateTime(1990, 4, 22), "PK", "BIM"),
+        new("Suneer Bista", new DateTime(2000, 6, 10), "Cenral TU", "BIT"),
+        new("Sita Acharya", new DateTime(2010, 8, 29), "CCRC", "BIT"),
+        new("Ram Basnet", new DateTime(2004, 9, 23), "People's College", "BSC"),
+        new("Rama Chitrakar", new DateTime(2003, 8, 17), "People's College", "BSC"),
+        new("Mahesh Manandhar", new DateTime(2004, 9, 23), "CCRC", "BIM"),
+        new("Renu Shahi", new DateTime(2004, 9, 23), "Vedas", "BIM"),
+        new("Sanjana Luitel", new DateTime(2007, 4, 26), "PK", "BSC")
+	];
+		
+	public static void LetsQuery()
+	{
+
+		// Get all trainees grouped by their college
+		
+        Console.WriteLine("Trainees grouped by College:");
+
+         var traineeGroupedByCollege = trainees.GroupBy(t => t.college);
+            foreach (var traineeGroup in traineeGroupedByCollege)
+            {
+                Console.WriteLine($"College: {traineeGroup.Key}");
+                foreach (var trainee in traineeGroup)
+                {
+                    Console.WriteLine($"Name: {trainee.name}");
+                }
+            }
+            Console.WriteLine();
+            
+
+		// Get name and age of trainees who are on their teens
+		
+         Console.WriteLine("Name and age of trainees on their teens:");
+
+            var teens = trainees
+                .Select(t => new { Name = t.name, Age = t.CalculateAge()})
+                .Where(teens => teens.Age.Item2 / 365 >= 13 && teens.Age.Item2 /365 <= 19);
+            foreach (var teen in teens)
+            {
+                Console.WriteLine($"{teen.Name}, Age: {teen.Age.Item1}");
+            }
+            Console.WriteLine();
+
+
+		// Get name initial, age and academic program of trainees and order result by initials
+		
+           Console.WriteLine("Trainees Ordered by Initials:");
+            var traineeOrderByInitials = trainees
+                .Select(t => new { Initials = t.GetInitials(), Age = t.CalculateAge().Item1, Program = t.program})
+                .OrderBy(t => t.Initials);
+            
+            foreach (var trainee in traineeOrderByInitials)
+            {
+                Console.WriteLine($"{trainee.Initials}=> Age: {trainee.Age}, Program: {trainee.Program}");
+            }
+
+
+	}
+}
---
+++ Assignment4/QueryCollection/Trainee.cs
@@ -0,0 +1,30 @@
+﻿// File - Trainee.cs
+internal class Trainee(string name, DateTime dob, string college, string program)
+{
+    public string name = name;
+    public DateTime dob = dob;
+    public string college = college;
+    public string program = program;
+
+    // Calculate age of a trainee in years and months e.g. 22 Years and 2 Months
+    public (string, int) CalculateAge()
+    {
+        var ageSpan = DateTime.Now - dob;
+        var year = ageSpan.Days / 365;
+        var months = ageSpan.Days % 365 / 30;
+        return ($"{year} Years and {months} Months", ageSpan.Days);
+    }
+
+    // Get name initials of a trainee e.g. Bishnu Singh Rawal => BR
+    public string GetInitials()
+    {
+        var nameParts = name.Split(" ");
+        char firstInitial = nameParts[0][0];
+        var lastInitial = nameParts[^1][0];   
+
+        return $"{firstInitial}{lastInitial}";       
+    }
+
+    public static string PrintDetails(Trainee trainee) => 
+		$"{trainee.name}\t{trainee.dob.ToShortDateString()}\t{trainee.college}\t{trainee.program}";   
+}
