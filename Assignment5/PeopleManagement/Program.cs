0b4360ff6fadb33f8192615c575803c3788b9f35
Saraswoti Pant <sspant01@gmail.com>
2024-08-29 12:52:55 +0545

    Assignment5

---
+++ Assignment5/PeopleManagement/Program.cs
@@ -0,0 +1,5 @@
+﻿﻿string filePath = @"D:\Git\TeksewaOOPFundamental\Assignment5\PeopleManagement";
+List<Person> people = await CSVParser.ParseCSV(filePath);
+await PeopleReport.SaveMales(people, filePath);
+await PeopleReport.SaveFemales(people, filePath);
+await PeopleReport.SaveDotComUsers(people, filePath);
